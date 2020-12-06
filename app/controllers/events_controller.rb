a1b35e37028d9f1939479f577a6fadfc2555f91e
zawawa0722 <sekaoza1213@gmail.com>
2020-12-06 13:51:23 +0900

    形だけ完成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,15 @@
 class EventsController < ApplicationController
+
+  before_action :move_to_index, except: [:index]
+
   def index
     @event = Event.all
+  end
+end
+
+private
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "entrances#index"
   get "events", to: "events#index"
   get "finances", to: "finances#show"
