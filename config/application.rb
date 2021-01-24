7b7c1da5889ca35c88ae3b7a1a5a2f92bc1bc6a5
zawawa0722 <sekaoza1213@gmail.com>
2021-01-24 17:28:12 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module CalendarApp2
     config.load_defaults 6.0
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
+    # config.assets.initialize_on_precompile = false
     # config.assets.precompile += ['application-print.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
